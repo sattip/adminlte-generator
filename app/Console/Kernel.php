d4e48510af6a96a99562b996b635cda0d26b32d4
George Pittas <sattip@hotmail.com>
2020-05-15 15:20:35 +0300

    run schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        App\Console\Commands\SyncOrders::class,
+        App\Console\Commands\UpdateOrders::class,
     ];
 
     /**
@@ -24,7 +25,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('sync:orders')->hourly();
+        $schedule->command('update:orders')->everyFifteenMinutes();
     }
 
     /**
